710c887547d7a3630877bec8661eea8bad9e9bfd
srinikon <skondepudi@gmail.com>
2017-12-01 13:43:28 -0800

    monty hall

--- ch2.R
+++ ch2.R
@@ -1,48 +1,45 @@
-# Chapter 1
+n <- 10^5
+child1 <- sample(2,n,replace=TRUE)
+child2 <- sample(2,n,replace=TRUE)
+# Here child1 is a vector of length n, where each element is a 1 or a 2. Letting 1 stand
+# for “girl” and 2 stand for “boy”, this vector represents the gender of the elder child
+# in each of the n families. Similarly, child2 represents the gender of the younger
+# child in each family.
+# Alternatively, we could have used
+sample(c("girl","boy"),n,replace=TRUE)
 
-# vectors
-v <- c(3,1,4,1,5,9)
-sum(v)
-v[2]
-v[c(1,3,5)]
-v[-(2:4)]
+n.b <- sum(child1==1) #child 1 is a girl
+n.ab <- sum(child1==1 & child2==1) # both children are girls
+n.ab/n.b # prob P(GG|child1=G)
 
-1/(1:100)^2
+n.b <- sum(child1==1 | child2==1)
+n.ab <- sum(child1==1 & child2==1)
+n.ab/n.b
 
-n <- 10; k <- 5
-sample(n,k)
-sample(4, 10000, replace=TRUE, prob=c(0.1,0.2,0.3,0.4))
+# Monty Hall simulation
+n <- 10^5
+cardoor <- sample(3,n,replace=TRUE)
 
-# combinations
-choose(n,k)
-# permutations
-choose(n,k) * factorial(k)
+monty <- function() {
+  doors <- 1:3
+  # randomly pick where the car is
+  cardoor <- sample(doors,1)
+  # prompt player
+  print("Monty Hall says ‘Pick a door, any door!’")
+  # receive the player’s choice of door (should be 1,2, or 3)
+  chosen <- scan(what = integer(), nlines = 1, quiet = TRUE)
+  # pick Monty’s door (can’t be the player’s door or the car door)
+  if (chosen != cardoor) montydoor <- doors[-c(chosen, cardoor)]
+  else montydoor <- sample(doors[-chosen],1)
+  # find out whether the player wants to switch doors
+  print(paste("Monty opens door ", montydoor, "!", sep=""))
+  print("Would you like to switch (y/n)?")
+  reply <- scan(what = character(), nlines = 1, quiet = TRUE)
+  # interpret what player wrote as "yes" if it starts with "y"
+  if (substr(reply,1,1) == "y") chosen <- doors[-c(chosen,montydoor)]
+  # announce the result of the game!
+  if (chosen == cardoor) print("You won!")
+  else print("You lost!")
+}
 
-#log(n!)
-lfactorial(k)
-lchoose(n,k)
-
-# Let’s show by simulation that the probability of a matching card is approximately 1 − 1/e when the deck is sufficiently large.
-n <- 100
-r <- replicate(10^4,sum(sample(n)==(1:n)))
-sum(r>=1)/10^4
-
-1 - 1/exp(1)
-
-# Birthday problem
-k <- 23
-1-prod((365-k+1):365)/365^k
-pbirthday(23) # returns the probability of at least one match if the room has k people
-qbirthday(0.5) # returns the number of people needed in order to have probability p of at least one match
-pbirthday(23)
-qbirthday(0.5)
-
-pbirthday(23,coincident=3)
-qbirthday(0.5,coincident=3)
-
-b <- sample(1:365,23,replace=TRUE)
-tabulate(b)
-
-r <- replicate(10^4, max(tabulate(sample(1:365,23,replace=TRUE))))
-sum(r>=2)/10^4
-
+monty()
